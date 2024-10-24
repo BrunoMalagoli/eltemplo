d75f66b34bfe69c17903cc64a692a17ebdf52d9a
Bruno Malagoli <bmalagoli2@gmail.com>
2024-10-23 22:14:02 -0300

    Fixed routing problems for prototype deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import CotizacionPage from "./pages/CotizacionPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/cotizacion",
-    element: <CotizacionPage />,
-  },
-]);
+const isProduction = window.location.hostname !== "localhost";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/cotizacion",
+      element: <CotizacionPage />,
+    },
+  ],
+  { basename: isProduction ? "/eltemplo" : "/" }
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
